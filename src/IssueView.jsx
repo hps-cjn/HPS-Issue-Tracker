3492759854e58ea00ef5b3ff482d92839795e5aa
cnascone <Chris.Nascone@highpointsolutions.com>
2016-03-11 15:57:19 -0500

    Issue view mockup (initial)

--- src/IssueView.jsx
+++ src/IssueView.jsx
@@ -1,14 +1,33 @@
 var React = require('react');
+var Icon = require('./Icon.jsx');
+var Button = require('./Button.jsx');
 
 var IssueView = React.createClass({
   render: function() {
     return (
       <div>
-        <div className='row'>
-          <h2>An Issue</h2>
+        <div className='row y-center'>
+          <h3>An Issue</h3>
         </div>
         <div className='row'>
-          <pre>{this.props.activeId}</pre>
+          <span className="label highlight">Open</span>
+          <span className="label alert">Defect</span>
+          <span className="label secondary">Low Priority</span>
+        </div>
+        <div className='row'>
+          <strong>Crystal Burt</strong> opened this issue 6 days ago.
+          <br></br>
+          Assigned to <strong>Eric Weintraub</strong>
+          <br></br>
+          Updated 03-10-2016, Due 03-10-2016
+        </div>
+        <div className='row'>
+          <p>Reprehenderit officia labore enim occaecat reprehenderit proident non cupidatat. Irure pariatur irure eiusmod exercitation Lorem anim ea laboris velit velit veniam nulla ad magna. Qui deserunt sint esse ea.</p>
+        </div>
+        <div className='row'>
+          <div className='large-4'>
+            <Button text='Comment' icon='pencil' />
+          </div>
         </div>
       </div>
     );
